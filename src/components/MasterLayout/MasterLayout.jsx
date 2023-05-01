c41774b75d25ee89d7c58fc72e8400744a1822e6
arif-adnan <arif751803@gmail.com>
2023-05-01 17:04:06 +0600

    onclick-dropdown & profile update_user profile done

--- src/components/MasterLayout/MasterLayout.jsx
+++ src/components/MasterLayout/MasterLayout.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useRef } from 'react';
+import React, {Fragment, useRef, useState} from 'react';
 import { Accordion, Container, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import {AiOutlineBank, AiOutlineLogout, AiOutlineMenu, AiOutlineUser} from 'react-icons/ai';
@@ -8,6 +8,13 @@ import logo from "../../assets/images/Logo.svg"
 import {getUserDetails, removeSessions} from "../../helper/SessionHelper";
 const MasterLayout = (props) => {
     let contentRef, sideNavRef,topNavRef = useRef();
+
+
+    // onclick dropdown visibility
+    const [isDropdownVisible, setDropdownVisibility] = useState(false);
+    function toggleDropdown() {
+        setDropdownVisibility(!isDropdownVisible);
+    }
 
     const MenuBarClickHandler = () => {
         let sideNav = sideNavRef;
@@ -256,36 +263,47 @@ const MasterLayout = (props) => {
         <Fragment>
 
             {/* top-nav */}
-            <Navbar className="fixed-top px-0 ">
+            <Navbar className="fixed-top px-0">
                 <Container fluid={true}>
-
                     <Navbar.Brand>
-                        <div ref={(div) => {topNavRef = div}} className="top-nav-open">
-                            <h4 className="text-white m-0 p-0"><a onClick={MenuBarClickHandler}><AiOutlineMenu /></a></h4>
+                        <div ref={(div) => { topNavRef = div }} className="top-nav-open">
+                            <h4 className="text-white m-0 p-0">
+                                <a onClick={MenuBarClickHandler}><AiOutlineMenu /></a>
+                            </h4>
                         </div>
                     </Navbar.Brand>
 
                     <div className="float-right h-auto d-flex align-items-center">
                         <div className="user-dropdown">
-                            <img className="icon-nav-img icon-nav" src={getUserDetails()['photo']} alt=""/>
-                            <div className="user-dropdown-content ">
-                                <div className="mt-4 text-center">
-                                    <img className="icon-nav-img" src={getUserDetails()['photo']} alt=""/>
-                                    <h6>{getUserDetails()['firstName']}</h6>
-                                    <hr className="user-dropdown-divider  p-0"/>
+                            <img
+                                className="icon-nav-img icon-nav"
+                                src={getUserDetails().photo}
+                                alt=""
+                                onClick={toggleDropdown}
+                            />
+                            {isDropdownVisible && (
+                                <div className="user-dropdown-content">
+                                    <div className="mt-4 text-center">
+                                        <img
+                                            className="icon-nav-img"
+                                            src={getUserDetails().photo}
+                                            alt=""
+                                        />
+                                        <h6>{getUserDetails().firstName}</h6>
+                                        <hr className="user-dropdown-divider  p-0" />
+                                    </div>
+                                    <NavLink to="/Profile" className="side-bar-item">
+                                        <AiOutlineUser className="side-bar-item-icon" />
+                                        <span className="side-bar-item-caption">Profile</span>
+                                    </NavLink>
+                                    <a onClick={onLogout} className="side-bar-item">
+                                        <AiOutlineLogout className="side-bar-item-icon" />
+                                        <span className="side-bar-item-caption">Logout</span>
+                                    </a>
                                 </div>
-                                <NavLink to="/Profile" className="side-bar-item">
-                                    <AiOutlineUser className="side-bar-item-icon" />
-                                    <span className="side-bar-item-caption">Profile</span>
-                                </NavLink>
-                                <a onClick={onLogout}  className="side-bar-item">
-                                    <AiOutlineLogout className="side-bar-item-icon" />
-                                    <span className="side-bar-item-caption">Logout</span>
-                                </a>
-                            </div>
+                            )}
                         </div>
                     </div>
-
                 </Container>
             </Navbar>
 
